e1bab1510f60439c96ac1be12152f36ad7da4014
yfchu <yfchu@Ctrip.com>
2016-06-28 20:39:18 +0800

    fix some bugs

---
+++ admin/menu.js
@@ -0,0 +1,15 @@
+[
+{
+     "title" : "用户管理",
+     "subMenus" : [
+     {"name":"管理1", "url":"main"},
+     {"name":"管理1", "url":"content"}
+     ]
+ },
+ {
+     "title" : "用户管理2",
+     "subMenus" : [
+     {"name":"管理2", "url":"content"}
+     ]
+ }
+]
