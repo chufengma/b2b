cae11a66a07bf9c6bd87f180c1e7dfd3c3b73912
yfchu <yfchu@Ctrip.com>
2016-08-08 09:53:28 +0800

    add user message port

--- src/main/java/onefengma/demo/server/config/ConfigBean.java
+++ src/main/java/onefengma/demo/server/config/ConfigBean.java
@@ -11,6 +11,7 @@ public class ConfigBean {
     public static String DATA_BASE_PASS = "8686239";
     public static String LOG_FILE_PREFIX = "/root/data/logs-dev/";
     public static int PORT = 80;
+    public static int USER_MESSAGE_PORT = 9091;
 
     public static void configDev() {
         DATA_BASE_URL = "jdbc:mysql://localhost:3306/b2b_dev?useUnicode=true&characterEncoding=utf8&useSSL=false";
@@ -18,6 +19,7 @@ public class ConfigBean {
         DATA_BASE_PASS = "8686239";
         LOG_FILE_PREFIX = "/root/data/logs-dev/";
         PORT = 9090;
+        USER_MESSAGE_PORT = 9092;
     }
 
     public static void configPro() {
@@ -26,6 +28,7 @@ public class ConfigBean {
         DATA_BASE_PASS = "8686239";
         LOG_FILE_PREFIX = "/root/data/logs/";
         PORT = 80;
+        USER_MESSAGE_PORT = 9091;
     }
 
 }
