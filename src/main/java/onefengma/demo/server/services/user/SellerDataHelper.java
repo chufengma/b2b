f2d5ebc2bbe54c87d193e1ae5297f1df726fc298
chufengma <chufengma@foxmail.com>
2016-08-16 21:33:13 +0800

    modify shop recommend numbers

--- src/main/java/onefengma/demo/server/services/user/SellerDataHelper.java
+++ src/main/java/onefengma/demo/server/services/user/SellerDataHelper.java
@@ -141,7 +141,7 @@ public class SellerDataHelper extends BaseDataHelper {
                 "from seller_transactions where productType in (0, 2) and finishTime < :endTime and finishTime >= :startTime " +
                 "group by sellerId) as trans " +
                 "on seller.userId = trans.sellerId where passed=true " +
-                "order by money desc limit 0, 10 ";
+                "order by money desc limit 0, 15 ";
             try(Connection conn = getConn()) {
                 List<Row> rows = conn.createQuery(sql)
                         .addParameter("startTime", DateHelper.getThisMonthStartTimestamp())
@@ -303,7 +303,7 @@ public class SellerDataHelper extends BaseDataHelper {
                 "from seller_transactions where productType in (1,3) and finishTime < :endTime and finishTime >= :startTime " +
                 "group by sellerId) as trans " +
                 "on seller.userId = trans.sellerId where passed=true " +
-                "order by money desc limit 0, 10 ";
+                "order by money desc limit 0, 15 ";
 
         try(Connection conn = getConn()) {
             List<Row> rows = conn.createQuery(sql)
